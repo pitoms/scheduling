b786df31d3f5e2d63d7ecf689eb5138f1fe97c6b
Pitom Saha <pitoms@gmail.com>
2025-03-14 20:40:27 -0400

    Remove unused files causing build errors.

--- app/confirm/student/page.tsx
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@/utils/supabase/server";
-
-export default async function ChooseUser() {
-  const supabase = await createClient();
-  const { data: users } = await supabase.from("test_users").select();
-
-  return <pre>{JSON.stringify(users, null, 2)}</pre>;
-}
